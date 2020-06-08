f447d6d4701e09fcfc158fed3697fa192abfe86d
Vishal Khatal <vishal.khatal2206@gmail.com>
2020-06-08 17:30:37 +0530

    Created skeleton

---
+++ ECommerce/Ecommerce.ProductCatelog.Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ecommerce.ProductCatelog.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Availability { get; set; }
+
+    }
+}
